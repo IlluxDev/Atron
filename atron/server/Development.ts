d8cce5c7d646dee2c38d4c9c5a8c1f97758fd022
XfaonAE <xfaonae@gmail.com>
2021-11-14 23:49:49 -0500

    Ignore cache

--- atron/server/Development.ts
+++ atron/server/Development.ts
@@ -2,19 +2,54 @@ import { exec } from "child_process";
 import path from "path";
 
 export class DevelopmentServer {
+	private electronDistLocation?: string;
+
 	public constructor() {
 		this.runElectron();
 	}
 
 	private runElectron() {
-		this.startElectronCompiler();
+		this.startElectronCompiler().then(() => {
+			console.log("TypeScript compiler is ready");
+
+			this.initializeElectronDistLocation().then(() => {
+				console.log("[ DEBUG ] Electron dist is at " + this.electronDistLocation);
+			});
+		});
 	}
 
-	private startElectronCompiler() {
-		const compileCommand = "npx tsc --watch --outDir ../.atron";
-		const atronLocation = path.join(process.cwd(), "./atron");
+	private initializeElectronDistLocation(): Promise<void> {
+		return new Promise(resolve => {
+			const atronLocation = path.join(process.cwd(), "./atron");
+			const distGetter = exec("node _GetElectron.js", { cwd: atronLocation });
 
-		const typescriptCompiler = exec(compileCommand, { cwd: atronLocation });
-		
+			distGetter.stdout?.on("data", data => {
+				this.electronDistLocation = data;
+				resolve();
+			});
+		});
+	}
+
+	private startElectronDevelopmentServer(): Promise<void> {
+		return new Promise(resolve => {
+
+		});
+	}
+
+	private startElectronCompiler(): Promise<void> {
+		return new Promise(resolve => {
+			const compileCommand = "npx tsc --watch --outDir ../.atron";
+			const atronLocation = path.join(process.cwd(), "./atron");
+			let initialStarted = false;
+
+			const typescriptCompiler = exec(compileCommand, { cwd: atronLocation });
+			typescriptCompiler.stdout?.on("data", () => !initialStarted 
+				? (() => { 
+					initialStarted = true;
+					resolve();
+				})()
+				: null
+			);
+		});
 	}
 }
