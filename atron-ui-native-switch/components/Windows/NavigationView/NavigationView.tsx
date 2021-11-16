831298ec9d880635ae266e145e7a2e203a463d3b
XfaonAE <xfaonae@gmail.com>
2021-11-16 02:16:52 -0500

    Added search bar

--- atron-ui-native-switch/components/Windows/NavigationView/NavigationView.tsx
+++ atron-ui-native-switch/components/Windows/NavigationView/NavigationView.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavigationViewProps } from "./NavigationViewProps";
 import styles from "./NavigationView.module.scss";
 import { Icon } from "@iconify/react";
 
 export function NavigationView(props: NavigationViewProps) {
+	const [sideBarOpened, setSideBarOpenedState] = useState(true);
+	
 	return (
 		<div className={styles.root}>
 			<div className={styles.header}>
@@ -17,8 +19,24 @@ export function NavigationView(props: NavigationViewProps) {
 			<div className={styles.mainArea}>
 				<div className={styles.sideBar}>
 					<div>
-						<button className={styles.sideBarButton}>
+						<button onClick={() => sideBarOpened ? setSideBarOpenedState(false) : setSideBarOpenedState(true)} className={styles.sideBarButton}>
 							<Icon icon="fluent:navigation-16-regular" />
+						</button>
+
+						<button className={`${styles.sideBarButton} ${sideBarOpened ? styles.sideBarButtonOpened : {}}`}>
+							{ !sideBarOpened ? <Icon icon="fluent:search-16-regular" /> : null }
+							{ sideBarOpened ? <input /> : null }
+						</button>
+					</div>
+
+					<div className={styles.sideBarContent}>
+						
+					</div>
+
+					<div>
+						<button className={`${styles.sideBarButton} ${sideBarOpened ? styles.sideBarButtonOpened : {}}`}>
+							<Icon icon="fluent:settings-16-regular" />
+							<span>Settings</span>
 						</button>
 					</div>
 				</div>
