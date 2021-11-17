cac97499ca41011266518215693f1cb787b0599f
XFaon <85759101+XFaonAE@users.noreply.github.com>
2021-11-17 18:58:41 -0500

    Dropdowns now have expand

--- atron-ui-native-switch/components/Windows/NavigationView/DropDownButton.tsx
+++ atron-ui-native-switch/components/Windows/NavigationView/DropDownButton.tsx
@@ -4,6 +4,8 @@ import { DropDownButtonProps } from "./DropDownButtonProps";
 import { Icon } from "@iconify/react";
 
 export function DropDownButton(props: DropDownButtonProps) {
+	const [isOpen, setIsOpenState] = useState(false);
+
 	return (
 		<div className={`${styles.root} ${props.innerTree && props.displayFull ? styles.innerTree : {}}`}>
 			<button className={`${styles.button} ${!props.displayFull ? styles.buttonHideText : {}}`}>
@@ -15,12 +17,12 @@ export function DropDownButton(props: DropDownButtonProps) {
 					<span>{props.label}</span>
 				</span>
 
-				<button>
+				{props.tree.length != 0 ? <div onClick={() => isOpen ? setIsOpenState(false) : setIsOpenState(true)}>
 					<Icon icon={"fluent:chevron-down-16-regular"} />
-				</button>
+				</div> : null }
 			</button>
 
-			{props.displayFull ? <div className={styles.list}>
+			{props.displayFull ? <div className={`${styles.dropDown} ${!isOpen ? styles.dropDownClosed : {}}`}>
 				{props.tree?.map(item => {
 					return <DropDownButton displayFull={props.displayFull} innerTree={true} label={item.label} tree={item.list ?? []} />
 				})}
